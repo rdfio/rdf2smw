957992c1fe4b2531e0cca41f11a77719c0d587fd
Samuel Lampa <samuel.lampa@gmail.com>
2017-04-10 16:01:53 +0200

    Split out TripleAggrFanOut in separate file

---
+++ tripleaggrfanout.go
@@ -0,0 +1,27 @@
+package main
+
+type FanOutTripleAggregate struct {
+	In  chan *TripleAggregate
+	Out map[string](chan *TripleAggregate)
+}
+
+// NewFanOut creates a new FanOut process
+func NewFanOutTripleAggregate() *FanOutTripleAggregate {
+	return &FanOutTripleAggregate{
+		In:  make(chan *TripleAggregate, BUFSIZE),
+		Out: make(map[string](chan *TripleAggregate)),
+	}
+}
+
+// Run runs the FanOut process
+func (proc *FanOutTripleAggregate) Run() {
+	for _, outPort := range proc.Out {
+		defer close(outPort)
+	}
+
+	for ft := range proc.In {
+		for _, outPort := range proc.Out {
+			outPort <- ft
+		}
+	}
+}
