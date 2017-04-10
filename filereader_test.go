e45ee646696f03f7f37df6c2249c4024e88ac8b3
Samuel Lampa <samuel.lampa@gmail.com>
2017-04-10 11:56:36 +0200

    Test channels work in file reader

--- filereader_test.go
+++ filereader_test.go
@@ -16,4 +16,21 @@ func TestNewFileReader(t *testing.T) {
 	if fr.OutLine == nil {
 		t.Error("In-port InFileName not initialized in New FileReader")
 	}
+
+	go func() {
+		fr.InFileName <- "teststring"
+	}()
+	teststr1 := <-fr.InFileName
+	if teststr1 != "teststring" {
+		t.Error("In-port InFileName is not a string channel")
+		fr.InFileName <- "teststring"
+	}
+
+	go func() {
+		fr.OutLine <- "teststring"
+	}()
+	teststr2 := <-fr.OutLine
+	if teststr2 != "teststring" {
+		t.Error("Out-port OutLine is not a string channel")
+	}
 }
