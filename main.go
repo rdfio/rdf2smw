46ec3bfcbd9f186b89179cb0735be58ea9254efd
Samuel Lampa <samuel.lampa@gmail.com>
2017-04-10 10:59:23 +0200

    Split filereader into separate file

--- main.go
+++ main.go
@@ -21,7 +21,6 @@ https://www.mediawiki.org/wiki/Manual:Importing_XML_dumps
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"io"
@@ -157,44 +156,6 @@ func main() {
 // ================================================================================
 // Components
 // ================================================================================
-
-// --------------------------------------------------------------------------------
-// FileReader
-// --------------------------------------------------------------------------------
-
-type FileReader struct {
-	InFileName chan string
-	OutLine    chan string
-}
-
-func NewFileReader() *FileReader {
-	return &FileReader{
-		InFileName: make(chan string, BUFSIZE),
-		OutLine:    make(chan string, BUFSIZE),
-	}
-}
-
-func (p *FileReader) Run() {
-	defer close(p.OutLine)
-
-	flowbase.Debug.Println("Starting loop")
-	for fileName := range p.InFileName {
-		flowbase.Debug.Printf("Starting processing file %s\n", fileName)
-		fh, err := os.Open(fileName)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer fh.Close()
-
-		sc := bufio.NewScanner(fh)
-		for sc.Scan() {
-			if err := sc.Err(); err != nil {
-				log.Fatal(err)
-			}
-			p.OutLine <- sc.Text()
-		}
-	}
-}
 
 // --------------------------------------------------------------------------------
 // TurtleFileReader
