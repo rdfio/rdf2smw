7038894a621f208919ad21fce73833a4da4cdb10
Samuel Lampa <samuel.lampa@gmail.com>
2017-04-11 10:52:13 +0200

    Add tests for MWXMLCreator

---
+++ mwxmlcreator_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/flowbase/flowbase"
+	"testing"
+)
+
+// TestNewMWXMLCreator tests NewMWXMLCreator
+func TestNewMWXMLCreator(t *testing.T) {
+	flowbase.InitLogDebug()
+
+	mxc := NewMWXMLCreator(true)
+
+	if mxc.InWikiPage == nil {
+		t.Error("InWikiPage is not initialized")
+	}
+	if mxc.OutTemplates == nil {
+		t.Error("OutTemplates is not initialized")
+	}
+	if mxc.OutProperties == nil {
+		t.Error("OutProperties is not initialized")
+	}
+	if mxc.OutPages == nil {
+		t.Error("OutPages is not initialized")
+	}
+	if mxc.UseTemplates != true {
+		t.Error("UseTemplates field is initialized wrongly")
+	}
+}
