eed2b5b0deebc29367ae53c5061a19e1c57c99d7
Samuel Lampa <samuel.lampa@gmail.com>
2017-04-11 14:37:08 +0200

    Fix GOPATH related errors in build scripts

--- testcov.sh
+++ testcov.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set -ex
 for pkg in "github.com/rdfio/rdf2smw" "github.com/rdfio/rdf2smw/components"; do
+    touch profile_tmp.cov
     go test -v -covermode=count -coverprofile=profile_tmp.cov $pkg || ERROR="Error testing $pkg"
     tail -n +2 profile_tmp.cov >> cover.out || exit "Unable to append coverage for $pkg"
 done
