12a5e97fefcfe72a956e2f85616bbd7188ba2e45
Samuel Lampa <samuel.lampa@gmail.com>
2017-04-11 13:33:51 +0200

    exit, not die, in bash

--- testcov.sh
+++ testcov.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for pkg in "github.com/rdfio/rdf2smw" "github.com/rdfio/rdf2smw/components"; do
     go test -v -covermode=count -coverprofile=profile_tmp.cov $pkg || ERROR="Error testing $pkg"
-    tail -n +2 profile_tmp.cov >> cover.out || die "Unable to append coverage for $pkg"
+    tail -n +2 profile_tmp.cov >> cover.out || exit "Unable to append coverage for $pkg"
 done
