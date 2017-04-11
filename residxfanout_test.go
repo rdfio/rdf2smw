16c4578ac35fa0d6c0df3de351b600aaa4a0c565
Samuel Lampa <samuel.lampa@gmail.com>
2017-04-11 09:02:45 +0200

    Add tests for resource-index fan-out component

---
+++ residxfanout_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/flowbase/flowbase"
+	"testing"
+)
+
+// TestNewResourceIndexFanOut tests NewResourceIndexFanOut
+func TestNewResourceIndexFanOut(t *testing.T) {
+	flowbase.InitLogDebug()
+
+	rif := NewResourceIndexFanOut()
+
+	if rif.In == nil {
+		t.Error("In-port not initialized with channel")
+	}
+	if rif.Out == nil {
+		t.Error("Out-port not initialized with map of channels")
+	}
+}
+
+func TestResourceIndexFanOut(t *testing.T) {
+	flowbase.InitLogDebug()
+
+	rif := NewResourceIndexFanOut()
+	rif.Out["out1"] = make(chan *map[string]*TripleAggregate)
+	rif.Out["out2"] = make(chan *map[string]*TripleAggregate)
+
+	resIdxInner := make(map[string]*TripleAggregate)
+	resIdx := &resIdxInner
+
+	go func() {
+		defer close(rif.In)
+		rif.In <- resIdx
+	}()
+	go rif.Run()
+
+	resIdx1 := <-rif.Out["out1"]
+	if resIdx1 == nil {
+		t.Error("Got nil as output from out1")
+	}
+	resIdx2 := <-rif.Out["out2"]
+	if resIdx2 == nil {
+		t.Error("Got nil as output from out2")
+	}
+}
